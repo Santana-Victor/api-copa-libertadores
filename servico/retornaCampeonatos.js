569fcf333ccc66ae0fc6f26c2acf83388241cf22
Victor Santana <santanavictor592@gmail.com>
2024-09-10 10:59:38 -0300

    refactor: refatorar as funções que retornão campeonatos

--- servico/retornaCampeonatos.js
+++ servico/retornaCampeonatos.js
@@ -1,53 +1,55 @@
 const pool = require('./conexao.js');
 
+async function executaQuery(conexao, query) {
+  const resultadoQuery = await conexao.query(query);
+
+  const resposta = resultadoQuery[0];
+
+  return resposta;
+}
+
 async function retornaCampeonatos() {
   const conexao = await pool.getConnection();
+  const query = 'SELECT * FROM Campeonatos';
 
-  const tabelaCampeonatos = await conexao.query('SELECT * FROM Campeonatos');
-  const campeonatos = tabelaCampeonatos[0];
+  const resposta = executaQuery(conexao, query);
 
   conexao.release();
 
-  return campeonatos;
+  return resposta;
 }
 
 async function retornaCampeonatoPeloId(id) {
   const conexao = await pool.getConnection();
+  const query = `SELECT * FROM Campeonatos WHERE id = ${id}`;
 
-  const tabelaCampeonatos = await conexao.query(
-    `SELECT * FROM Campeonatos WHERE id = ${id}`
-  );
-  const campeonato = tabelaCampeonatos[0];
+  const resposta = executaQuery(conexao, query);
 
   conexao.release();
 
-  return campeonato;
+  return resposta;
 }
 
 async function retornaCampeonatoPeloAno(ano) {
   const conexao = await pool.getConnection();
+  const query = `SELECT * FROM Campeonatos WHERE ano = ${ano}`;
 
-  const tabelaCampeonatos = await conexao.query(
-    `SELECT * FROM Campeonatos WHERE ano = ${ano}`
-  );
-  const campeonato = tabelaCampeonatos[0];
+  const resposta = executaQuery(conexao, query);
 
-  conexao.release;
+  conexao.release();
 
-  return campeonato;
+  return resposta;
 }
 
 async function retornaCampeonatoPeloTimeVencedor(time) {
   const conexao = await pool.getConnection();
+  const query = `SELECT * FROM Campeonatos WHERE campeao = '${time}'`;
 
-  const tabelaCampeonatos = await conexao.query(
-    `SELECT * FROM Campeonatos WHERE campeao = '${time}'`
-  );
-  const campeonato = tabelaCampeonatos[0];
+  const resposta = executaQuery(conexao, query);
 
-  conexao.release;
+  conexao.release();
 
-  return campeonato;
+  return resposta;
 }
 
 module.exports = {
